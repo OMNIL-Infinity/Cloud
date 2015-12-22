dd5bbfb07befcd9d1128015ddecb17bfb2ea9def
Kevin Khoa Pho <kevinkhoapho@yahoo.com>
2015-12-22 12:12:14 -0800

    005]Fix

--- VersionControl.bat
+++ VersionControl.bat
@@ -1,16 +1,6 @@
 @echo off
 cd %~dp0
-if not [%~2] == [] goto arg
-:returnarg
 git sync
 git add -A
-git commit -m %~1%~2
+git commit -m %~1
 git push origin master
-goto eof
-
-:arg
-set arg=" -m %~2"
-echo " -m %~2"
-goto returnarg
-
-:eof
